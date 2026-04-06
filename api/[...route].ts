34ff5725813bd439cb54c7b033e6bea7d2883d59
haruaki220 <haruaki.1115.watanabe@gmail.com>
2026-04-07 00:01:01 +0900

    vercel用にhonoにbasepathを設定

--- api/[...route].ts
+++ api/[...route].ts
@@ -5,9 +5,7 @@ import { cors } from "hono/cors";
 import { createSupabaseClient } from "./supabaseClient.js";
 import type { Env } from "./types.js";
 
-const app = new Hono<Env>();
-
-console.log("URL:", process.env.SUPABASE_URL);
+const app = new Hono<Env>().basePath("/api");
 
 app.use("*", cors());
 
