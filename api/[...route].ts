fba12d4355cd1aabea277b6084c7ef508f7fac60
haruaki220 <haruaki.1115.watanabe@gmail.com>
2026-04-07 22:55:33 +0900

    具体的なルートを先に移動

--- api/[...route].ts
+++ api/[...route].ts
@@ -27,6 +27,44 @@ app.use("/*", async (c, next) => {
   await next();
 });
 
+app.get("/study/summary", async (c) => {
+  const supabase = c.get("supabase");
+  const span = c.req.query("span");
+  let res;
+  if (span === "day") {
+    res = await supabase.rpc("get_daily_summary");
+  } else if (span === "week") {
+    res = await supabase.rpc("get_weekly_summary");
+  } else if (span === "month") {
+    res = await supabase.rpc("get_monthly_summary");
+  } else if (span === "year") {
+    res = await supabase.rpc("get_yearly_summary");
+  } else if (!span) {
+    return c.json({ message: "span is required" }, 400);
+  }
+  if (res) {
+    if (res.error) {
+      return c.json({ message: res.error.message }, 400);
+    } else {
+      return c.json(res.data);
+    }
+  }
+});
+
+app.get("/study/subject_summary", async (c) => {
+  const supabase = c.get("supabase");
+  const start_date = c.req.query("start_date");
+  const end_date = c.req.query("end_date");
+  const { data, error } = await supabase.rpc("get_subject_summary", {
+    start_date: start_date,
+    end_date: end_date,
+  });
+  if (error) {
+    return c.json({ message: error.message }, 400);
+  }
+  return c.json(data);
+});
+
 app.get("/study", async (c) => {
   const supabase = c.get("supabase");
 
@@ -35,9 +73,6 @@ app.get("/study", async (c) => {
     return c.json({ message: error.message }, 400);
   }
   return c.json(data);
-
-  // const result = await db.query("SELECT * FROM study_records");
-  // return c.json(result.rows);
 });
 
 app.post("/study", async (c) => {
@@ -61,11 +96,6 @@ app.post("/study", async (c) => {
   }
 
   return c.json(data);
-  // await db.query("INSERT INTO study_records (subject, minutes, memo) VALUES ( $1, $2, $3 )",
-  //   [body.subject, body.minutes, body.memo]
-  // );
-
-  // return c.json(body);
 });
 
 app.delete("/study/:id", async (c) => {
@@ -77,15 +107,12 @@ app.delete("/study/:id", async (c) => {
     .from("study_records")
     .delete()
     .eq("id", id)
-    .eq("user_id",user.id);
+    .eq("user_id", user.id);
 
   if (error) {
     return c.json({ message: error.message }, 400);
   }
   return c.json(data);
-  // await db.query("DELETE FROM study_records WHERE id = $1", [id]);
-
-  // return c.json({ message: "Deleted" });
 });
 
 app.put("/study/:id", async (c) => {
@@ -104,63 +131,13 @@ app.put("/study/:id", async (c) => {
       user_id: user.id,
     })
     .eq("id", id)
-    .eq("user_id",user.id)
+    .eq("user_id", user.id);
 
   if (error) {
     return c.json({ message: error.message }, 400);
   }
 
   return c.json(data);
-
-  // await db.query(
-  //   "UPDATE study_records SET subject = $1, minutes = $2, memo = $3 where id = $4",
-  //   [body.subject, body.minutes, body.memo, id],
-  // );
-
-  // return c.json(body);
 });
-
-app.get("/study/summary", async (c) => {
-  const supabase = c.get("supabase");
-  const span = c.req.query("span");
-  let res;
-  if(span==="day"){
-    res = await supabase.rpc("get_daily_summary");
-  }
-  else if(span==="week"){
-    res = await supabase.rpc("get_weekly_summary");
-  }
-  else if(span==="month"){
-    res = await supabase.rpc("get_monthly_summary");
-  }
-  else if(span==="year"){
-    res = await supabase.rpc("get_yearly_summary");
-  }
-  else if(!span){
-    return c.json({message:"span is required"}, 400);
-  }
-  if(res){
-    if(res.error){
-      return c.json({message: res.error.message}, 400)
-    }
-    else{
-      return c.json(res.data);
-    }
-  }
-})
-
-app.get("/study/subject_summary", async (c) => {
-  const supabase = c.get("supabase");
-  const start_date = c.req.query("start_date")
-  const end_date = c.req.query("end_date")
-  const {data, error} = await supabase.rpc("get_subject_summary", {
-    start_date:start_date,
-    end_date:end_date
-  })
-  if(error){
-    return c.json({message:error.message}, 400)
-  }
-  return c.json(data);
-})
 
 export default app;
