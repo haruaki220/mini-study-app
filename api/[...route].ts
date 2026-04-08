6c5bd79249a3f62220d1ec38b6f691c2f75df3dd
haruaki220 <haruaki.1115.watanabe@gmail.com>
2026-04-08 15:57:09 +0900

    export default handle(app)に変更

--- api/[...route].ts
+++ api/[...route].ts
@@ -5,6 +5,7 @@ import { cors } from "hono/cors";
 // import { createSupabaseClient } from "./supabaseClient.ts";
 import type { Env } from "./types.ts";
 import { createClient } from "@supabase/supabase-js";
+import { handle } from "hono/vercel";
 
 const createSupabaseClient = (accessToken: string) => {
   return createClient(
@@ -155,9 +156,11 @@ app.put("/study/:id", async (c) => {
   return c.json(data);
 });
 
-export default app;
+export default handle(app);
 
-export const GET = app.fetch;
-export const POST = app.fetch;
-export const PUT = app.fetch;
-export const DELETE = app.fetch;
+// export default app;
+
+// export const GET = app.fetch;
+// export const POST = app.fetch;
+// export const PUT = app.fetch;
+// export const DELETE = app.fetch;
